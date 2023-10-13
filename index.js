472762b086a4f6972fa3a4010a48548bf756ad12
Al4n-R1b31r0 <alan.ribeiro@lbv.org.br>
2023-10-13 12:47:37 -0300

    criando código assincrono

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import fs from  "fs";
+import fs from "fs";
 import chalk from "chalk";
 
 function trataErro(erro) {
@@ -8,14 +8,25 @@ function trataErro(erro) {
 function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8'
 
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro){
-            console.log(erro);
-            trataErro(chalk.red(erro.code, 'não há arquivo no diretório'))
-        } else {
-            console.log(chalk.green(texto));
-        }
-    })
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
 }
+
+/*
+    function pegaArquivo(caminhoDoArquivo) {
+        const encoding = 'utf-8'
+
+        fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+            if (erro){
+                console.log(erro);
+                trataErro(chalk.red(erro.code, 'não há arquivo no diretório'))
+            } else {
+                console.log(chalk.green(texto));
+            }
+        })
+    }
+*/
 
 pegaArquivo('./arquivos/texto.md')
