08ad7456163fb6ed6bd141612c1ed12fd82d20c4
Al4n-R1b31r0 <alan.ribeiro@lbv.org.br>
2023-10-16 12:31:00 -0300

    adicionando expressão regular para pegar os links do texto base

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import fs from "fs";
 import chalk from "chalk";
 
+const textoTeste = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).[ de retorno 400](https://httpstat.us/404).[gatinho salsicha](http://gatinhosalsicha.com.br/)"
+
+function extraiLinks(texto) {
+    const regex = /\[[^[\]]*?\]\(https?:\/\/[^\s?#.].[^\s]*\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultado = capturas.map(captura => 
+        ({[captura[1]] : captura[0]}));
+    return resultado;
+}
+
 function trataErro(erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'))
@@ -12,13 +22,13 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto))
     } catch (erro) {
         trataErro(erro)
     } 
 }
 
-/* Promises com then()
+/*Promises com then()
 
     function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8'
@@ -27,8 +37,6 @@ async function pegaArquivo(caminhoDoArquivo) {
     .readFile(caminhoDoArquivo, encoding)
     .then((texto) => console.log(chalk.green(texto)))
     .catch(trataErro)
-}
-*/
+}*/
 
 pegaArquivo('./arquivos/texto.md')
-pegaArquivo('./arquivos/')
