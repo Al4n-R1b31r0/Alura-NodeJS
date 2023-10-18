5b36eeec4a880f04b74f5437d9367320a8b854e2
Al4n-R1b31r0 <alan.ribeiro@lbv.org.br>
2023-10-18 10:51:40 -0300

    refinando o código do arquivo cli.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import fs from "fs";
 import chalk from "chalk";
 
-const textoTeste = "A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).[ de retorno 400](https://httpstat.us/404).[gatinho salsicha](http://gatinhosalsicha.com.br/)"
-
 function extraiLinks(texto) {
-    const regex = /\[[^[\]]*?\]\(https?:\/\/[^\s?#.].[^\s]*\)/gm;
+    const regex = /\[([^[\]]*?)\]\(https?:\/\/[^\s?#.].[^\s]*\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultado = capturas.map(captura => 
         ({[captura[1]] : captura[0]}));
-    return resultado;
+    return resultado.length !== 0 ? resultado : 'Não há links no arquivo!';
 }
 
 function trataErro(erro) {
@@ -22,7 +20,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(extraiLinks(texto))
+        return extraiLinks(texto)
     } catch (erro) {
         trataErro(erro)
     } 
