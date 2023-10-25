996da98a6cb0bc8d1de62e81e9b8ac9004906cfe
Al4n-R1b31r0 <alan.ribeiro@lbv.org.br>
2023-10-25 11:01:49 -0300

    terminando a api de verificação de links

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,35 +1,41 @@
-import chalk from "chalk"
+import chalk from "chalk";
 
-function extraiLinks(arrLinks) {
-    return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
+function extraiLinks (arrLinks) {
+  return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
-async function checaStatus(listaURLs) {
-    const arrStatus = await Promise.all(
-        listaURLs.map(async (url) =>{
-            try{
-                const response = await fetch(url)
-                return response.status
-            } catch (erro){
-                return manejaErros(erro)
-            }
-        })
-    )
-    return arrStatus
+async function checaStatus (listaURLs) {
+  const arrStatus = await Promise
+  .all(
+    listaURLs.map(async (url) => {
+      try {
+        const response = await fetch(url)
+        return response.status;
+      } catch (erro) {
+        return manejaErros(erro);
+      }
+    })
+  )
+  return arrStatus;
 }
 
 function manejaErros (erro) {
-    console.log(chalk.red('Algo deu Errado', erro));
+  if (erro.cause.code === 'ENOTFOUND') {
+    return 'link não encontrado';
+  } else {
+    return 'ocorreu algum erro';
+  }
 }
 
-export default async function listaValida(listaDeLinks) {
-    const links =extraiLinks(listaDeLinks)
-    const status = await checaStatus(links);
+export default async function listaValidada (listaDeLinks) {
+  const links = extraiLinks(listaDeLinks);
+  const status = await checaStatus(links);
 
-    return listaDeLinks.map((objeto, indice) => ({
-        ...objeto,
-        status: status[indice]
-    }))
+  return listaDeLinks.map((objeto, indice) => ({
+    ...objeto,
+    status: status[indice]
+  }))
 }
+
 
 //[gatinho salsicha](http://gatinhosalsicha.com.br/)
